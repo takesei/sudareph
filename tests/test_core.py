18890abcac5486e01c38fcfd7da9b8d3a8fda913
takesei <sei06k14@gmail.com>
2024-03-29 02:36:33 +0900

    :rotating_light: Add tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,4 @@
-from sudareph import Data, Flow, work_cls, work_fn
+from sudareph import Data, Flow, Parallel, work_cls, work_fn
 
 
 def test_class_decorator():
@@ -29,3 +29,55 @@ def test_fn_decorator():
     res = work('ASDF')
     assert isinstance(res, Data)
     assert res.output == 'POST:ASDF'
+
+
+def test_single_flow():
+    @work_cls('Sum')
+    class Summation:
+        def __init__(self, prefix: str):
+            self.prefix = prefix
+
+        def __call__(self, str1: str) -> str:
+            return self.prefix + str1
+
+    work = Summation('POST:')
+    input = Data('name', 'value')
+
+    result = input > work
+
+    assert result.output == 'POST:value'
+
+
+def test_multi_flow():
+    @work_cls('Sum')
+    class Summation:
+        def __init__(self, prefix: str):
+            self.prefix = prefix
+
+        def __call__(self, str1: str) -> str:
+            return self.prefix + str1
+
+    pipeline = Summation('PRE:') >> Summation('POST:')
+    res = Data('name', 'value') > pipeline
+
+    assert res.output == 'POST:PRE:value'
+
+
+def test_parallel_flow():
+    @work_cls('Sum')
+    class Summation:
+        def __init__(self, prefix: str):
+            self.prefix = prefix
+
+        def __call__(self, str1: str) -> str:
+            return self.prefix + str1
+
+    pipeline = Summation('Init:') >> Parallel(
+        'Parallel',
+        A=Summation('A1:') >> Summation('A2:'),
+        B=Summation('B1:') >> Summation('B2:'),
+    )
+
+    res = Data('name', 'ARG') > pipeline
+
+    assert res.output == dict(A='A2:A1:Init:ARG', B='B2:B1:Init:ARG')
